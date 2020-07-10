34b1adf90ebef9b4bd6c1e54acec2b4cc1e6a1d3
aerialcombat <djkim24601@gmail.com>
2020-07-10 21:21:24 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "DropDownMenuKit",
+    // platforms: [.iOS("9.0")],
+    products: [
+        .library(name: "DropDownMenuKit", targets: ["DropDownMenuKit"])
+    ],
+    targets: [
+        .target(
+            name: "DropDownMenuKit",
+            path: "Source"
+        )
+    ]
+)
